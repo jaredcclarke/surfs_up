551a41bf334b8357736ff0e550dd21e875a6806d
Jared Clarke <jared@Jareds-Air.fios-router.home>
2020-11-22 15:29:57 -0500

    final

--- app(practice).py
+++ app(practice).py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
     def hello_world():
-        return 'Hello world'
+       return 'Hello world'
 
 @app.route('/name')
     def name():
