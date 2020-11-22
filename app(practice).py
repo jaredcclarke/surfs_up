cffa55f34eb1ed26ca22e3e683c544cba7d26cdf
Jared Clarke <jared@Jareds-Air.fios-router.home>
2020-11-21 20:13:32 -0500

    practice

---
+++ app(practice).py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+    def hello_world():
+        return 'Hello world'
+
+@app.route('/name')
+    def name():
+        return 'Jared'
+
+app.route('/age')
+    def age():
+        return '32'
